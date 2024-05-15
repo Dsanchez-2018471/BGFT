ccc3521e8c9162354ae641665d356e51759c0306
Dsanchez-2018471 <dsanchez-2018471@kinal.edu.gt>
2024-05-15 17:49:38 -0600

    S

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -6,10 +6,11 @@ export const AboutPage = () => {
   return (
     <div className="about-container">
       <h1 className="about-title">Proyecto del blog</h1>
-      <p className="about-description">"Plataforma Digital para la Documentación y Compartición de Actividades Escolares" es un proyecto 
-      concebido para servir como un medio formal y organizado para registrar, compartir y reflexionar sobre las actividades realizadas por los 
-      estudiantes durante el transcurso del año académico. Esta iniciativa tiene como objetivo proporcionar un espacio digital estructurado y 
-      seguro donde los estudiantes puedan documentar sus experiencias educativas, proyectos y logros de manera sistemática y accesible.</p>
+      <p className="about-description">El proyecto "Plataforma Digital para la Documentación y
+        Compartición de Actividades Escolares" busca crear un espacio en línea seguro donde los
+        estudiantes puedan registrar y compartir sus actividades escolares. La idea es tener un
+        lugar estructurado para documentar proyectos, logros y experiencias educativas de manera
+        accesible y sistemática durante el año académico.</p>
     </div>
   );
 }
